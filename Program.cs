ff690f1539d4884bf0b3fb5644dc4829db3dd0e9
pablo <palmierijpablo@gmail.com>
2022-05-23 14:30:34 -0300

    avances pto4 string

--- Program.cs
+++ Program.cs
@@ -14,18 +14,67 @@ Console.WriteLine("Hello World!");
 // Console.WriteLine(numero);
 
 // 4.- Dada una cadena de texto ingresada por el usuario, realice las siguientes tarea:
-Console.WriteLine("Ingrese una una frase:");
+Console.WriteLine("\nIngrese una una frase:");
 string cadena = Console.ReadLine();
 // ● Obtener algunas letras que forman parte de una cadena
 char c1 = cadena[0];
 char c5 = cadena[4];
-Console.WriteLine("\nLetras 1 y 5: " + c1 + c5);
+Console.WriteLine("\n1.- Letras 1 y 5: " + c1 + c5);
 // ● Obtener la longitud de la cadena y muestre por pantalla
-Console.WriteLine("\nLongitud: " + cadena.Length);
+Console.WriteLine("\n2.- Longitud: " + cadena.Length);
 // ● Concatenar (Unir) dos cadenas distintas
 string cadena2 = " y otra cadena";
-Console.WriteLine("\nConcatenada: " + cadena + cadena2);
+Console.WriteLine("\n3.- Concatenada: " + cadena + cadena2);
 // ● Extraer una subcadena
-Console.WriteLine("\nSubcadena: " + cadena.Substring(1,3));
+Console.WriteLine("\n4.- Subcadena: " + cadena.Substring(1,3));
 // ● Utilizando la calculadora creada anteriormente realizar las operaciones de dos
 // números y mostrar por pantalla en texto el resultado.
+// int op;
+// double a, b;
+//     do {
+//         Console.WriteLine("\n5.- Ingrese el código de la operación: ");
+//         op = Convert.ToInt16(Console.ReadLine());
+//     } while (op==0 || op>4);
+//     Console.WriteLine("Ingrese el primer número: ");
+//     a = Convert.ToDouble(Console.ReadLine());
+//     Console.WriteLine("Ingrese el segundo número: ");
+//     b = Convert.ToDouble(Console.ReadLine());
+//     Console.WriteLine("El resultado de " + calculadora(op,a,b));
+// ● Recorrer la cadena de texto con un ciclo Foreach e ir mostrando elemento por
+// elemento en pantalla
+Console.WriteLine("\n6.- Bucle foreach: ");
+foreach (var i in cadena) {
+    Console.Write(i + " ");
+}
+// ● Buscar la ocurrencia de una palabra determinada en la cadena ingresada
+// ● Convierta la cadena a Mayúsculas y luego minúsculas
+// ● Ingrese una nueva cadena y luego compare los valores de ambas.
+// ● Ingrese una cadena separada por caracteres que usted determine y muestre por
+// pantalla los resultados (Revisar el comportamiento de split())
+// ● Siguiendo con el ejemplo de la calculadora (ejercicio 2) ingrese una ecuación
+// simple como cadena de caracteres y que el sistema lo resuelva. Por ej. ingrese
+// por pantalla “582+2” y que le devuelva la suma de 582 con 2
+
+
+
+
+
+
+
+
+string calculadora(int op, double a, double b)
+{
+    switch (op) {
+        case 1: return a + " + " + b + " es " + (a+b);
+        case 2: return a + " - " + b + " es " + (a-b);
+        case 3: return a + " * " + b + " es " + (a*b);
+        case 4: 
+            if(b!=0) {
+                return a + " / " + b + " es " + (a/b);
+            } else {
+                return a + " / " + b + " es infinito.";
+            }
+        default: 
+            return "la operación es no valído.";
+    }
+}
